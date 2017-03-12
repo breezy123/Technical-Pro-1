5fa48568d64d676dc2e8089d5d7b6807f2ffa7ce
breezy123 <aufredericks@gmail.com>
2017-03-12 00:42:38 -0800

    project first part completed...

---
+++ src/Calculator.java
@@ -0,0 +1,41 @@
+/**
+ * Created by aubrey on 12/03/2017.
+ */
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args)
+    {
+        int num1=0, num2=0, answer=0;
+        char choice;
+        Scanner keyIn = new Scanner(System.in);
+
+        System.out.print("Choose one of the Calculations to Perform: 1-addition, 2-subtraction \n");
+        choice = keyIn.next().charAt(0);
+
+        if(choice == '1')
+        {
+            System.out.println("Please Enter First Number: ");
+            num1 = keyIn.nextInt();
+            System.out.println("Enter Second Number: ");
+            num2 = keyIn.nextInt();
+
+            answer = num1 + num2;
+        }
+        else if(choice =='2')
+        {
+            System.out.println("Please Enter First Number: ");
+            num1 = keyIn.nextInt();
+            System.out.println("Enter Second Number: ");
+            num2 = keyIn.nextInt();
+
+            answer = num1 - num2;
+        }
+        else
+        {
+            System.out.println("Invalid Selection Made...");
+        }
+
+
+        System.out.print("Answer is: " + answer);
+    }
+}
